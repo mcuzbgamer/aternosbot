9337fc2939d07af9d6740d283fb90da980f04bac
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-10-09 22:46:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-    host: 'mineteamuz.aternos.me',
-    port: 55637,
-    version: 1.17,
-    username: 'Afk_miner'
-})
-
+  host:'    host: 'mineteamuz.aternos.me',
+  // port: 55637,
+  username: 'Afk_miner'
+ })
+ 
+ let continue_digging = true; 
+ 
 bot.once("spawn", () => {
   bot.chat("/login komiljon");
 })
 
-bot.on('bot',(username,message)=>{
-  if(message == 'salom'){
-  bot.chat('Serverga xush kelibsiz ${username}')
-  })
+
+bot.on('chat', (username, message) => {
+  if (message == 'digyoungerbot') {
+    continue_digging=true;
+    dig()  
+  }
+})
+
+async function dig() {
+  if(!continue_digging){return};
+  if (!bot.heldItem || !bot.heldItem.name.includes('pickaxe')) {
+    var pickaxe = bot.inventory.items().filter(i => i.name.includes('pickaxe'))[0]; 
+  if (pickaxe) await bot.equip(pickaxe, 'hand')
+  }
+  var block = bot.blockAtCursor(4);
+  if (!block) return setTimeout (function () { dig(); }, 100);
+  await bot.dig(block, 'ignore')
+  dig()
+}
